abee59e30ae8913dabf92274a06e74b18c48f0fb
Nedudigital <segunchinedu2000@gmail.com>
2025-07-15 13:46:34 +0100

    Initial commit of Shopify Inventory Webhook project

---
+++ app/api/create-webhook/route.js
@@ -0,0 +1,19 @@
+export async function GET() {
+  const response = await fetch('https://armadillotough.com/admin/api/2023-07/webhooks.json', {
+    method: "POST",
+    headers: {
+      "X-Shopify-Access-Token": process.env.SHOPIFY_ADMIN_API_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      webhook: {
+        topic: "inventory_levels/update",
+        address: "https://shopify-inventory-webhooks.vercel.app/api/inventory-update",
+        format: "json",
+      },
+    }),
+  });
+
+  const data = await response.json();
+  return new Response(JSON.stringify(data), { status: 200 });
+}
